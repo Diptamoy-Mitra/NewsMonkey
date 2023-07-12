24f60941fa4c6fab2c42d0517c98f2b5f63d3f2b
diptamoy mitra <mitradiptamoy70@gmail.com>
2023-07-13 02:15:02 +0530

    refactoring

--- src/components/News.js
+++ src/components/News.js
@@ -175,7 +175,7 @@ export class News extends Component {
     console.log("render")
     return (
       <div className='container my-3' >
-        <h1 className="text-center" style={{ margin: '35px 0px' }}>
+        <h1 className="text-center" style={{ margin: '35px 0px' ,marginTop:'90px'}}>
           NewsMonkey - Top  {this.capitalizeFirstLetter(this.props.category)} Headlines
         </h1>
 
